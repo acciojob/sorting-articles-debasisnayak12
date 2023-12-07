9e7026cd3054b9c8c89519a7770000b585fafdc9
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-07 11:34:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-//your JS code here. If required.
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const ignoredWords = ['a', 'an', 'the'];
+const ignoreWords = ['a','an','the'];
 
 function compareArticles(article1,article2) {
-	const comArticle1  = comArticle1.toLowerCase().slpit(' ').filter(word => !ignoredWords.includes(word)).join(' '); 
-	const comArticle2  = comArticle2.toLowerCase().slpit(' ').filter(word => !ignoredWords.includes(word)).join(' ');  
-    if(comArticle1 < comArticle2){
+    const comArticle1 = article1.toLowerCase().split(' ').filter(word => !ignoreWords.includes(word)).join(' ');	
+    const comArticle2 = article2.toLowerCase().split(' ').filter(word => !ignoreWords.includes(word)).join(' ');
+
+	if(comArticle1 < comArticle2){
 		return -1;
 	} else if(comArticle1 > comArticle2){
 		return 1;
-	} else{
+	}else{
 		return 0;
 	}
 }
-const sortedBands = bands.sort(compareArticles);
 
-alert(sortedBands);    
-  
+const sortedBands = bands.sort(compareArticles);
+// alert(sortedBands)
+
 const ul = document.getElementById('bands');
-sortedBands.forEach((band) => {
+sortedBands.forEach((bands) => {
 	const listItems = document.createElement('li');
-	listItems.textContent = band;
+	listItems.textContent = bands;
 	ul.appendChild(listItems);
 });
+
